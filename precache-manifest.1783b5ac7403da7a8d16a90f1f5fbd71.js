b406d3b4ec4d02c3371bb2ca88ef9f6ae6dbf09a
Akash <akash@akash.com>
2019-10-12 12:38:40 +0530

    Updates

---
+++ precache-manifest.1783b5ac7403da7a8d16a90f1f5fbd71.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c252ddd35b8b25a5199b8c1c07670c",
+    "url": "/MyProfile/index.html"
+  },
+  {
+    "revision": "50c6e3287ecb4e00fb05",
+    "url": "/MyProfile/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "4ef5c91e17989b79312b",
+    "url": "/MyProfile/static/css/main.5d500845.chunk.css"
+  },
+  {
+    "revision": "50c6e3287ecb4e00fb05",
+    "url": "/MyProfile/static/js/2.fc1e47cd.chunk.js"
+  },
+  {
+    "revision": "4ef5c91e17989b79312b",
+    "url": "/MyProfile/static/js/main.3b10ce4c.chunk.js"
+  },
+  {
+    "revision": "926b1085874dbc1c2206",
+    "url": "/MyProfile/static/js/runtime~main.f71baf26.js"
+  },
+  {
+    "revision": "1454e8de8172103bef04abd7e5158f7b",
+    "url": "/MyProfile/static/media/git.1454e8de.svg"
+  },
+  {
+    "revision": "e20a68edc84140f2869b2c078ada2d41",
+    "url": "/MyProfile/static/media/latest_photo.e20a68ed.jpg"
+  },
+  {
+    "revision": "64050d629ebdc24fb490aaf3a3577e20",
+    "url": "/MyProfile/static/media/linkedIn.64050d62.svg"
+  },
+  {
+    "revision": "c191eeb333d4ad80ff63a30978c4fa04",
+    "url": "/MyProfile/static/media/spinner.c191eeb3.svg"
+  },
+  {
+    "revision": "61d85715898356e7a524e35804b96daf",
+    "url": "/MyProfile/static/media/theme.61d85715.svg"
+  }
+]);
