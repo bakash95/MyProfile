72c9e83fa4e2772973313d6fba824448eba3d4ce
Akash <akash@akash.com>
2020-02-29 22:07:11 +0530

    updated homepage text

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,7 +7,7 @@ const Home = (props) =>(
         <h4 className="border-btm">Hi, This is Akash</h4>
         <section>
             <article className="line-1_7">
-                This is a Application which i Hope will Showcase My Technical Expertise made with ReactJS and Spring Boot Backend,The WebApp will be responsive with a option to Switch themes and customized Scrollbars, you can view my resume and set of projects under my belt and a link to my LinkedIN and Github profile..
+                This is a Application made with ReactJS ,The WebApp will be responsive with a option to Switch themes, you can view my resume and set of projects under my belt and a link to my LinkedIN and Github profile..
             </article>
         </section>
     </div>
