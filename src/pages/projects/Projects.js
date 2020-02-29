cad13823e57008ab42bed61a01feda150f15b349
Akash <akash@akash.com>
2020-02-29 20:46:19 +0530

    project added and externalized

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import projects from './projectList'
+
 import './css/projects.css'
 
 export default class Projects extends Component {
@@ -12,36 +14,16 @@ export default class Projects extends Component {
     render() {
         return (
             <section className="container">
-                <article className="cardContainer">
-                    <div className="cardText">
-                        <summary className="cardTextHeader">Hospital Mgmt</summary>
-                        <p className="cardTextContent">Plain Spring Boot Application with API gateway with URL whitelisting and role based Login with JWT and custom claims</p>
-                    </div>
-                </article>
-                <article className="cardContainer">
-                    <div className="cardText">
-                        <summary className="cardTextHeader">NewsBulletin</summary>
-                        <p className="cardTextContent">A web Application that uses plain vanilla JS for displaying news articles</p>
-                    </div>
-                </article>
-                <article className="cardContainer">
-                    <div className="cardText">
-                        <summary className="cardTextHeader">Lending Loans</summary>
-                        <p className="cardTextContent">A ReactJS Application that displays and filters loans lent to users and has a responsive UI and a powerful search utilising Hibernate Search for filtering and sorting with spring data for storing and retrieving data from a Mysql DB</p>
-                    </div>
-                </article>
-                <article className="cardContainer">
-                    <div className="cardText">
-                        <summary className="cardTextHeader">BlogApp</summary>
-                        <p className="cardTextContent">A BlogApp which is to show articles and also allow to post articles along with a provision to list based on authors</p>
-                    </div>
-                </article>
-                <article className="cardContainer">
-                    <div className="cardText">
-                        <summary className="cardTextHeader">Spring Security based Login</summary>
-                        <p className="cardTextContent">A Spring MVC application with Spring security backed login function</p>
-                    </div>
-                </article>
+                {
+                    projects.map((project) =>
+                        <article className="cardContainer">
+                            <div className="cardText">
+                                <summary className="cardTextHeader">{project.title}</summary>
+                                <p className="cardTextContent">{project.content}</p>
+                            </div>
+                        </article>
+                    )
+                }
             </section>
         )
     }
---
+++ src/pages/projects/projectList.js
@@ -0,0 +1,28 @@
+const projects = [
+    {
+        title: 'Hospital Mgmt',
+        content: 'Plain Spring Boot Application with API gateway with URL whitelisting and role based Login with JWT and custom claims'
+    },
+    {
+        title: 'NewsBulletin',
+        content: 'A web Application that uses plain vanilla JS for displaying news articles'
+    },
+    {
+        title: 'Lending Loans',
+        content: 'A ReactJS Application that displays and filters loans lent to users and has a responsive UI and a powerful search utilising Hibernate Search for filtering and sorting with spring data for storing and retrieving data from a Mysql DB'
+    },
+    {
+        title: 'BlogApp',
+        content: 'A BlogApp which is to show articles and also allow to post articles along with a provision to list based on authors'
+    },
+    {
+        title: 'Spring Security based Login',
+        content: 'A Spring MVC application with Spring security backed login function'
+    },
+    {
+        title: 'Speech keep App',
+        content: 'A speech keep app which allows to add speeches to be written and view/edit added speeches'
+    }
+]
+
+export default projects;
