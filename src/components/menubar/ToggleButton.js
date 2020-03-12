702e9053a433add4153a851cd82c7a08fc3d53af
Akash <akash@akash.com>
2020-03-13 01:22:46 +0530

    aria fix

--- src/components/menubar/ToggleButton.js
+++ src/components/menubar/ToggleButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './css/togglebutton.css'
 
 const ToggleButton = (props)=>(
-    <button className="toggler" onClick={props.pageActions.toggleDrawer}>
+    <button className="toggler" aria-label="toggle menu" onClick={props.pageActions.toggleDrawer}>
         <div className="toggle_line"></div>
         <div className="toggle_line"></div>
         <div className="toggle_line"></div>
